48501ca0f5ef191b46672e0a95dadc6b0c2c400c
Sattvik Palta <sattvikpalta@gmail.com>
2022-06-04 13:56:10 -0400

    Adds API integration

---
+++ src/CardList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ bots }) => {
+  return (
+    <div>
+      { 
+        bots.map((user, i) => {
+          return (
+            <Card 
+              key={ i } 
+              id={ bots[i].id } 
+              name={ bots[i].name } 
+              email={ bots[i].email } 
+              />
+          );
+        })
+      }
+  </div>
+  );
+}
+
+export default CardList;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import CardList from './CardList';
 import { bots } from './bots';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
-ReactDOM.render(
-                <div>
-                  <Card id={bots[0].id} name={bots[0].name} email={bots[0].email}/>
-                  <Card id={bots[1].id} name={bots[1].name} email={bots[1].email}/>
-                  <Card id={bots[2].id} name={bots[2].name} email={bots[2].email}/>
-                </div>
-                , document.getElementById('root'));
+ReactDOM.render(<CardList bots = { bots } />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bi   t.ly/CRA-vitals
 reportWebVitals();
