ab59f2c6439d48ffe19ab0676f2cdb7b55192314
Sattvik Palta <Sattvik@Sattviks-MacBook-Pro.local>
2022-06-04 13:18:22 -0400

    Adds UI to botmates

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './Card';
+import { bots } from './bots';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
-ReactDOM.render(<Card/>, document.getElementById('root'));
+ReactDOM.render(
+                <div>
+                  <Card id={bots[0].id} name={bots[0].name} email={bots[0].email}/>
+                  <Card id={bots[1].id} name={bots[1].name} email={bots[1].email}/>
+                  <Card id={bots[2].id} name={bots[2].name} email={bots[2].email}/>
+                </div>
+                , document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
