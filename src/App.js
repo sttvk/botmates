380323c6696d822a51eff029465dbe595fdddc08
Sattvik Palta <sattvikpalta@gmail.com>
2022-06-06 15:33:31 -0400

    Fetches data from REST API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { bots } from './bots';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      bots: bots,
+      bots: [],
       searchfield: ''
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ bots: users }));
   }
 
   onSearchChange = (event) => {
