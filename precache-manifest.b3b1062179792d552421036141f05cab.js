2bfda13707cbae22e0333c53ebbf7e8f726ebf66
Sattvik Palta <sattvikpalta@gmail.com>
2022-06-06 17:43:31 -0400

    Updates

---
+++ precache-manifest.b3b1062179792d552421036141f05cab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "23d636943f3ba70d1f5d",
+    "url": "/botmates/static/css/main.13976f2f.chunk.css"
+  },
+  {
+    "revision": "23d636943f3ba70d1f5d",
+    "url": "/botmates/static/js/main.2930a483.chunk.js"
+  },
+  {
+    "revision": "6a8cd5bf6a0eeb7116da",
+    "url": "/botmates/static/js/runtime~main.742ad647.js"
+  },
+  {
+    "revision": "5dbd2061b9a667659e52",
+    "url": "/botmates/static/css/2.9c83bb77.chunk.css"
+  },
+  {
+    "revision": "5dbd2061b9a667659e52",
+    "url": "/botmates/static/js/2.738473dc.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/botmates/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "7a413610c4288ee37b0b9d5e29eafee2",
+    "url": "/botmates/static/media/SEGA.7a413610.woff"
+  },
+  {
+    "revision": "5fee87c9a8dac814b7282f37c4c84dc3",
+    "url": "/botmates/index.html"
+  }
+];
