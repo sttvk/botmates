1d0b2361b9772471566c44d9a54fa02141c31c60
Sattvik Palta <sattvikpalta@gmail.com>
2022-06-06 16:26:21 -0400

    Updates to React v18

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
 reportWebVitals();
